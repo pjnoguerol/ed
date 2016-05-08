ddd54b82f2f094ed3eb2aa6561088182efd57879
Pablo Javier Noguerol Santalices <Pablo Javier Noguerol Santalices>
2016-05-08 10:28:51 +0200

    UML

---
+++ uml/Calendario.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+
+
+public class Calendario {
+   
+    private String enfermeVacunacion;
+
+    
+    private Date fechaVacunacion;
+
+    
+    public String getEnfermeVacunacion() {
+    }
+
+    
+    public void setEnfermeVacunacion(String enferme) {
+    }
+
+    
+    public Date getFechaVacunacion() {
+    }
+
+    
+    public void setFechaVacunacion(Date fecha) {
+    }
+
+}
---
+++ uml/Historial.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+
+
+public class Historial {
+   
+    private String enfermedad;
+
+    
+    private Date fecha;
+
+    
+    public String getEnfermedad() {
+    }
+
+    
+    public void setEnfermedad(String enfermedad) {
+    }
+
+    
+    public Date getFecha() {
+    }
+
+    
+    public void setFecha(String fecha) {
+    }
+
+}
